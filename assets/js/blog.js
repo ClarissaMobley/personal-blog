5dfa1c2d9138c29d16d1ffdd319b648a7cc5d0c5
Clarissa Mobley <clarissamobley@me.com>
2024-03-28 10:27:58 -0400

    added to Java and HTML

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,16 @@
-// Add event listener to the back button
-document.getElementById('backButton').addEventListener('click', function() {
-    // Go back to index.html
-    history.back();
-});
+// Add event listener to the back button if it exists
+const backButton = document.getElementById('backButton');
+if (backButton) {
+    backButton.addEventListener('click', function() {
+        // Go back to index.html
+        history.back();
+    });
+}
 
+const username = localStorage.getItem('addUsername');
+const title = localStorage.getItem('addTitle');
+const content = localStorage.getItem('addContent');
+
+document.getElementById('addUserName').textContent = username;
+document.getElementById('addTitle').textContent = title;
+document.getElementById('addContent').textContent = content;
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,33 +1,25 @@
-const username = document.getElementById('username');
-const title = document.getElementById('title');
-const content = document.getElementById('content');
+    const form = document.getElementById('form');
+    const username = document.getElementById('username');
+    const title = document.getElementById('title');
+    const content = document.getElementById('content');
 
-function saveBlogPost() {
-    // Save related form datat as an object
-    const blogPost = {
-        username: username.value,
-        title: title.value,
-        content: content.value.trim()
-    };
-    // Use .setItem() to store object in local storage
-    localStorage.setItem('blogPost', JSON.stringify(blogPost));
-}
 
-function renderBlogPost() {
-    const lastPost = JSON.parse(localStorage.getItem('blogPost'));
-    if (lastPost !== null) {
-        document.getElementById('last-username').innerHTML = lastPost.username;
-        document.getElementById('last-title').innerHTML = lastPost.title;
-        document.getElementById('last-content').innerHTML = lastPost.content;
-    }
-}
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
 
-document.getElementById("form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-    
-    // Save the blog post
-    saveBlogPost();
-    
-    // Redirect to the "blog.html" page
-    window.location.href = "blog.html";
-});
+        const usernameValue = username.value;
+        const titleValue = title.value;
+        const contentValue = content.value;
+
+        localStorage.setItem('addUsername', usernameValue);
+        localStorage.setItem('addTitle', titleValue);
+        localStorage.setItem('addContent', contentValue);
+
+        window.location.href = 'blog.html';
+
+        // Clear the form fields after submit
+        username.value = '';
+        title.value = '';
+        content.value = '';
+        
+    });
