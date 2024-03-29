bd1542c863a8c53f7c5c4802640f81868d76acbc
Clarissa Mobley <clarissamobley@me.com>
2024-03-28 20:24:50 -0400

    Added alert and fixed bugs

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 
 # Personal Blog
 
-I was suppose to create an interactive blog website. I created this project with HTML, CSS, and JavaScript. 
-
+I took on the challenge of building an interactive blog website, using HTML, CSS, and JavaScript. With these tools, I crafted a user-friendly platform where visitors can browse and engage with content effortlessly. HTML provided the structure, CSS added style, and JavaScript introduced interactivity, making the website dynamic and engaging. This project allowed me to combine coding skills with creativity to deliver a functional and enjoyable online experience for users.
 
 ## Screenshots
 
+
+
+## 🔗 Links
+https://clarissamobley.github.io/personal-blog/
 
 
 ## Authors
@@ -17,10 +20,10 @@ I was suppose to create an interactive blog website. I created this project with
 ## Built With
 
 VSCode
-HTML
-CSS
-JavaScript
 
-## 🔗 Links
-[![website](https://img.shields.io/badge/webbsite-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://clarissamobley.github.io/personal-blog/)
+HTML
+
+CSS
+
+JavaScript
 
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,30 +1,36 @@
-    const form = document.getElementById('form');
-    const username = document.getElementById('username');
-    const title = document.getElementById('title');
-    const content = document.getElementById('content');
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const title = document.getElementById('title');
+const content = document.getElementById('content');
 
-    // Function to save blog post to local storage
-    function saveBlogPost() {
-     const blogHistory = JSON.parse(localStorage.getItem("blogPost")) || []
+// Function to save blog post to local storage
+function saveBlogPost() {
+    const blogHistory = JSON.parse(localStorage.getItem("blogPost")) || [];
 
-        // Create new blog post object
-        const blogPost = {
-            username: username.value,
-            title: title.value,
-            content: content.value.trim(),
-        };
-        
-        // Add new blog post at the beginning of the list
-        blogHistory.unshift(blogPost);
+    // Create new blog post object
+    const blogPost = {
+        username: username.value,
+        title: title.value,
+        content: content.value.trim()
+    };
 
-        localStorage.setItem('blogPost', JSON.stringify(blogHistory));
-    }
+    // Add new blog post at the beginning of the list
+    blogHistory.unshift(blogPost);
 
-    // Add event listener to submit form
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        saveBlogPost();
+    localStorage.setItem('blogPost', JSON.stringify(blogHistory));
 
+    return blogPost;
+}
+
+// Add event listener to submit form
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const blogPost = saveBlogPost();
+
+    if (blogPost.username === '' || blogPost.title === '' || blogPost.content === '') {
+        alert('Please fill out all fields.');
+    } else {
         window.location.href = 'blog.html';
-
-    });
+    }
+});
